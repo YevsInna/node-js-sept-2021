0917746ec3c0df5b0b0af7aa73cf6a97626a4196
YevsInna <evseinka@gmail.com>
2022-02-18 16:46:36 +0200

    lesson 2 home work

--- lesson2_hw/app.js
+++ lesson2_hw/app.js
@@ -1,14 +1,7 @@
-
-
 // 2. /users просто сторінка з усіма юзерами, але можна по квері параметрам їх фільтрувати по age і city
-// 3. /user/:id сторінка з інфою про одного юзера
-
-
 const express = require('express');
 const path = require('path');
 const {engine} = require('express-handlebars');
-
-const users = [];
 
 const app = express();
 
@@ -20,26 +13,55 @@ app.set('view engine', '.hbs');
 app.engine('hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
+const users = [];
+let error = '';
 
 app.get('/login', (req, res) => {
     res.render('login')
 });
-app.get('/users', (req, res) => {
-    res.render('users', {users})
+app.get('/users', ({query}, res) => {
+    if (Object.keys(query).length) {
+        let usersWithQuery = [...users];
+        if (query.city) {
+            usersWithQuery = usersWithQuery.filter(user => user.city === query.city);
+        }
+        if (query.age) {
+            usersWithQuery = usersWithQuery.filter(user => user.age === query.age)
+        }
+        res.render('users', {users: usersWithQuery});
+        return;
+    }
+    ;
+    res.render('users', {users});
 });
-app.get('/users/:userId',(req,res)=>{
-    const {userId} = req.params
-    res.json(users[userId])
-})
+// 3. /user/:id сторінка з інфою про одного юзера
+app.get('/users/:userId', ({params}, res) => {
+    const user = users.find(user => user.id === +params.userId);
+    if (!user) {
+        error = `User with ID # ${params.userId}  is not found!`;
+        res.redirect('/error');
+        return;
+    }
+    res.render('user', {user});
+});
 // просто зробити темплейт з цим усім і вводити свої дані які будуть пушитися в масив і редірект робити на сторінку
 // з усіма юзерами /users і перевірка чи такий імейл не існує, якщо існує то редірект на еррор пейдж
-app.post('/login', (req, res) => {
-
-    users.push(req.body)
+app.post('/login', ({body}, res) => {
+    const isUserAlreadyExist = users.some(user => user.email === body.email);
+    if (isUserAlreadyExist) {
+        error = 'Such email already exist!  Sign up with another email.';
+        res.redirect('error');
+        return;
+    }
+    users.push({...body, id: users.length ? users[users.length - 1].id + 1 : 1})
     res.redirect('/users')
 });
 
-app.use((req,res)=>{
+app.get('/error', (req, res) => {
+    res.render('error', {error});
+});
+
+app.use((req, res) => {
     res.render('notFound')
 })
 app.listen(5200, () => {
