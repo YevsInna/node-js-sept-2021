91633039ed02971de51b484984c93146dc42373f
YevsInna <evseinka@gmail.com>
2022-02-19 17:44:11 +0200

    lesson 2 home work delete

--- lesson2_hw/app.js
+++ lesson2_hw/app.js
@@ -17,53 +17,72 @@ const users = [];
 let error = '';
 
 app.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login');
 });
 
 app.get('/users', ({query}, res) => {
+
     if (Object.keys(query).length) {
         let usersWithQuery = [...users];
+
         if (query.city) {
             usersWithQuery = usersWithQuery.filter(user => user.city === query.city);
         }
+        ;
+
         if (query.age) {
             usersWithQuery = usersWithQuery.filter(user => user.age === query.age)
         }
+        ;
+
         res.render('users', {users: usersWithQuery});
+
         return;
     }
+    ;
     ;
     res.render('users', {users});
 });
 // 3. /user/:id сторінка з інфою про одного юзера
 app.get('/users/:userId', ({params}, res) => {
+
     const user = users.find(user => user.id === +params.userId);
+
     if (!user) {
         error = `User with ID # ${params.userId}  is not found!`;
         res.redirect('/error');
         return;
     }
+    ;
+
     res.render('user', {user});
 });
-app.delete('/users/:userId', ({query}, res) => {
-    let index = users.findIndex(user => user.id === query.id);
-    if (index){
+
+app.post('/users/:userId', ({params}, res) => {
+
+    let index = users.findIndex(user => user.id === params.id);
+
+    if (index) {
         users.splice(index, 1);
         res.redirect('/users');
-        return
-    };
-
+        return;
+    }
+    ;
 });
 // просто зробити темплейт з цим усім і вводити свої дані які будуть пушитися в масив і редірект робити на сторінку
 // з усіма юзерами /users і перевірка чи такий імейл не існує, якщо існує то редірект на еррор пейдж
 app.post('/login', ({body}, res) => {
+
     const isUserAlreadyExist = users.some(user => user.email === body.email);
+
     if (isUserAlreadyExist) {
         error = 'Such email already exist!  Sign up with another email.';
         res.redirect('error');
         return;
     }
-    users.push({...body, id: users.length ? users[users.length - 1].id + 1 : 1})
+    ;
+
+    users.push({...body, id: users.length ? users[users.length - 1].id + 1 : 1});
     res.redirect('/users')
 });
 
@@ -72,24 +91,29 @@ app.get('/error', (req, res) => {
 });
 
 app.get('/signIn', (req, res) => {
-    res.render('signIn')
+    res.render('signIn');
 });
 
-app.post('/signIn',({body},res)=>{
+app.post('/signIn', ({body}, res) => {
+
     const user = users.find(user => user.email === body.email && user.password === body.password);
-    if (!user){
-       error = 'Incorrect data entered!';
-       res.redirect('error');
-       return;
+
+    if (!user) {
+        error = 'Incorrect data entered!';
+        res.redirect('error');
+        return;
     }
-    res.redirect(`/users/${user.id}`)
-})
+    ;
+
+    res.redirect(`/users/${user.id}`);
+});
 
 app.use((req, res) => {
-    res.render('notFound')
-})
+    res.render('notFound');
+});
+
 app.listen(5200, () => {
-    console.log('Server started on PORT 5200')
+    console.log('Server started on PORT 5200');
 });
 
 // Необхідно розширити ваше ДЗ:
